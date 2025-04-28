2b091d665fff6543f891941344b9b273c6730c34
MaysStar <123299437+MaysStar@users.noreply.github.com>
2025-04-28 23:54:53 +0300

    corrective changes

--- Database_CPP/ObjectsName.cpp
+++ Database_CPP/ObjectsName.cpp
@@ -11,7 +11,7 @@ void ObjectsName::getData()
     sf::String text = "ObjectsName";
     object_text.setString(text);
 
-    object_background.setPosition(800, 85);
+    object_background.setPosition(870, 85);
 
     object_text.setOrigin(object_text.getLocalBounds().width / 2, object_text.getLocalBounds().height / 2);
     object_text.setPosition(object_background.getPosition());
